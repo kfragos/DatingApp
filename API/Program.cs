514b9a7a1c5268a0bd96491a52f99b0e3c406fbc
kfragos <kfragos@cubit.gr>
2022-06-17 12:51:57 +0300

    Finish the Skeleton for API

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Connection string to connect with Database 
+string ConnectionStr = builder.Configuration.GetConnectionString("Default");
+
+// Add DbContext
+builder.Services.AddDbContext<DataContext>(options => options.UseMySql(ConnectionStr, ServerVersion.AutoDetect(ConnectionStr)));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +23,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+   var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
+    context.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
