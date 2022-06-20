eb7ceff89d30aa8e2349ffd32516655bd714d9c3
kfragos <kfragos@cubit.gr>
2022-06-20 15:58:02 +0300

    Using HTTPS and add ssl certificate

--- API/Program.cs
+++ API/Program.cs
@@ -39,7 +39,7 @@ app.UseHttpsRedirection();
 app.UseCors(x => x.AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200"));
+    .WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
