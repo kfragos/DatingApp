b2b800621a59f487a35c3450d227afbe7978ac28
kfragos <kfragos@cubit.gr>
2022-06-21 15:29:41 +0300

    Add UserDto and return token

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs
+{
+    // This is the object that we return when a user login or register
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
