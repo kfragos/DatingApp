b3ca196a909e9d1ffd88e6708b5a70726d5a8c72
kfragos <kfragos@cubit.gr>
2022-06-23 13:39:48 +0300

    Loggin and Register Changes

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -9,21 +9,35 @@ import { User } from '../_models/user';
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
-  private currentUserSource = new ReplaySubject<User>(1); //(1)= how many previoys values you wwant ti rember
+  private currentUserSource = new ReplaySubject<User>(1); //(1)= how many previous values you want to remember
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
+  //User Log In
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
       })
     )
+  }
+
+  //Register a User
+  //model san parametros gia to response pou pernoume 
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register',model).pipe(
+      map((user : User) => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
   }
 
   setCurrentUser(user: User){
