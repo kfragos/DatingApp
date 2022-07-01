78851649afba4f6872dc4a7fb00160df1954b323
kfragos <kfragos@cubit.gr>
2022-07-01 15:44:26 +0300

    Client side registration adds

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validator, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -13,8 +14,9 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
-  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router ) { }
 
   ngOnInit(): void {
     this.intitializeForm();
@@ -44,14 +46,11 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe(response => {
-    //   console.log(response);
-    //   this.cancel();
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // });
+     this.accountService.register(this.registerForm.value).subscribe(response => {
+       this.router.navigateByUrl('/members');
+     }, error => {
+       this.validationErrors = error;
+     });
   }
 
   cancel(){
