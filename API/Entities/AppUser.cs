83b903995cd78227957052dc85d4c6c5fb128b25
kfragos <kfragos@cubit.gr>
2022-07-05 12:49:56 +0300

    Setting Entitiesfor messaging + Repository

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -21,5 +21,7 @@ namespace API.Entities
         public ICollection<Photo> Photos { get; set; } = new List<Photo>();
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
